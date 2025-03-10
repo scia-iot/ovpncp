c6e33fc2f42711a5c50729a2f8eab06e64932092
Nick <ipaddicting@gmail.com>
2025-03-10 05:24:03 +0000

    Added init steps.

--- sciaiot/ovpncp/dependencies.py
+++ sciaiot/ovpncp/dependencies.py
@@ -1,9 +1,19 @@
+import logging
+import os
 from sqlmodel import Session, SQLModel, create_engine
 
-sqlite_file_name = "ovpncp.db"
+logger = logging.getLogger(__name__)
+
+app_directory = "/opt/ovpncp"
+sqlite_file_name = f"{app_directory}/ovpncp.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 connect_args = {"check_same_thread": False}
 engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
+
+def create_app_directory():
+    if not os.path.exists(app_directory):
+        os.makedirs(app_directory)
+        logger.info(f"Created the app directory: {app_directory}")
 
 
 def get_session():
@@ -13,7 +23,9 @@ def get_session():
 
 def create_tables():
     SQLModel.metadata.create_all(engine)
+    logger.info("Created all tables.")
 
 
 def drop_tables():
     SQLModel.metadata.drop_all(engine)
+    logger.info("Dropped all tables.")
--- sciaiot/ovpncp/main.py
+++ sciaiot/ovpncp/main.py
@@ -1,18 +1,21 @@
+import importlib.resources
 import logging
 from contextlib import asynccontextmanager
 
 import uvicorn
 from fastapi import FastAPI
 
-from sciaiot.ovpncp.dependencies import create_tables
+from sciaiot.ovpncp.dependencies import create_app_directory, create_tables
 from sciaiot.ovpncp.routes import client, network, server
 
+log_config_path = importlib.resources.files("sciaiot.ovpncp").joinpath("log.yml")
 logger = logging.getLogger(__name__)
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # startup
+    create_app_directory()
     create_tables()
     logger.info("Startup events finished.")
 
@@ -21,11 +24,11 @@ async def lifespan(app: FastAPI):
     # shutdown
     logger.info("Shutdown events finished.")
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(redirect_slashes=False, lifespan=lifespan)
 app.include_router(server.router, prefix="/server", tags=["server"])
 app.include_router(client.router, prefix="/clients", tags=["client"])
 app.include_router(network.router, prefix="/networks", tags=["server"])
 
 
 def run():
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000, log_config=str(log_config_path))
